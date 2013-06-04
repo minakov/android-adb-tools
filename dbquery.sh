e143a525cfc12f32e70b00a11597b48931465b26
minakov <minakov@gmail.com>
2013-06-04 13:38:44 +0300

    Update dbquery.sh

--- dbquery.sh
+++ dbquery.sh
@@ -10,8 +10,9 @@ if [ $# -ne 1 ]
 fi;
 
 if [ -z "$ADB" ]; then ADB=adb; fi
+if [ -z "$ANDROID_HOME" ]; then ANDROID_HOME=$HOME/android-sdk-linux_86; fi
 $ADB -d shell "run-as $PACKAGE cat /data/data/$PACKAGE/databases/$DATABASE > /sdcard/${PACKAGE}_${DATABASE}"
 $ADB pull "/sdcard/${PACKAGE}_${DATABASE}" $HOME/"${PACKAGE}_${DATABASE}"
 $ADB -d shell "rm /sdcard/${PACKAGE}_${DATABASE}"
-$HOME/android-sdk-linux_86/tools/sqlite3 -header -column $HOME/${PACKAGE}_${DATABASE} "$1"
+$ANDROID_HOME/tools/sqlite3 -header -column $HOME/${PACKAGE}_${DATABASE} "$1"
 rm $HOME/"${PACKAGE}_${DATABASE}"
