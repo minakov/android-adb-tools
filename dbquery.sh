fbacb4c6ca5c86afcce7820f24cf02754169245b
minakov <minakov@gmail.com>
2013-06-04 13:07:18 +0300

    Create dbquery.sh

---
+++ dbquery.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -z "$ADB" ]; then ADB=adb; fi
+
+if [ $# -ne 1 ]
+ then
+  echo Usage: $(basename "$0") sql
+  exit 1
+fi;
+
+PACKAGE=
+DATABASE=
+
+$ADB -d shell "run-as $PACKAGE cat /data/data/$PACKAGE/databases/$DATABASE > /sdcard/${PACKAGE}_${DATABASE}"
+$ADB pull "/sdcard/${PACKAGE}_${DATABASE}" $HOME/"${PACKAGE}_${DATABASE}"
+$ADB -d shell "rm /sdcard/${PACKAGE}_${DATABASE}"
+$HOME/android-sdk-linux_86/tools/sqlite3 -header -column $HOME/${PACKAGE}_${DATABASE} "$1"
+rm $HOME/"${PACKAGE}_${DATABASE}"
